fafd61970d8360241e9e9331cca9d9e15eee6e9d
Ansgar Hiller <ansgar@weigelstein.de>
2017-08-31 18:16:25 +0200

    demo form Signed-off-by: Ansgar Hiller <ansgar@weigelstein.de>

--- public/ws/js/vendor/vendor.min.js
+++ public/ws/js/vendor/vendor.min.js
@@ -1,15 +1,14 @@
 /*!
  * ws-joomla v2.0.0
- * Compiled 2017-08-29 by Ansgar Hiller <ansgar@weigelstein.de> (www.weigelstein.de)
+ * Compiled 2017-08-31 by Ansgar Hiller <ansgar@weigelstein.de> (www.weigelstein.de)
  * 
  * Included vendors:
  * --------------------------------------------------
  * – jquery/1.12.4/jquery.js
- * – tether/1.3.3/tether.js
- * – chosen/1.5.1/chosen.jquery.min.js
  * – bootstrap/4.0.0/beta/popper.min.js
  * – bootstrap/4.0.0/beta/bootstrap.js
  * – chartjs/2.5.0/Chart.js
+ * – chosen/1.5.1/chosen.jquery.min.js
  * – gsap/1.19.0/TweenMax.js
  * – imagesloaded/3.1.4/imagesloaded.js
  * – scrollmagic/2.0.5/ScrollMagic.js
